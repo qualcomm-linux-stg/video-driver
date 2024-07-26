6333a4ae62d6e8c0e38f89149817f7005fd6cdc0
Vikash Garodia <quic_vgarodia@quicinc.com>
2024-07-26 12:54:50 +0530

    video: driver: fix number of concurrent sessions for VP9
    VP9 can support more than 6 sessions, and to allow the same,
    the limiting check is removed.
    
    Change-Id: Iaba99f2d0afa1ade15dcb39f60a6cbce47f36ada
    Signed-off-by: Vikash Garodia <quic_vgarodia@quicinc.com>

--- vidc/src/msm_vdec.c
+++ vidc/src/msm_vdec.c
@@ -2221,24 +2221,6 @@ int msm_vdec_try_fmt(struct msm_vidc_inst *inst, struct v4l2_format *f)
 	return rc;
 }
 
-static bool msm_vidc_check_max_sessions_vp9d(struct msm_vidc_core *core)
-{
-	u32 vp9d_instance_count = 0;
-	struct msm_vidc_inst *inst = NULL;
-
-	core_lock(core, __func__);
-	list_for_each_entry(inst, &core->instances, list) {
-		if (is_decode_session(inst) &&
-			inst->fmts[INPUT_PORT].fmt.pix_mp.pixelformat ==
-				V4L2_PIX_FMT_VP9)
-			vp9d_instance_count++;
-	}
-	core_unlock(core, __func__);
-
-	if (vp9d_instance_count > MAX_VP9D_INST_COUNT)
-		return true;
-	return false;
-}
 
 int msm_vdec_s_fmt(struct msm_vidc_inst *inst, struct v4l2_format *f)
 {
@@ -2257,16 +2239,6 @@ int msm_vdec_s_fmt(struct msm_vidc_inst *inst, struct v4l2_format *f)
 			rc = msm_vdec_codec_change(inst, f->fmt.pix_mp.pixelformat);
 			if (rc)
 				goto err_invalid_fmt;
-		}
-
-		if (f->fmt.pix_mp.pixelformat == V4L2_PIX_FMT_VP9) {
-			if (msm_vidc_check_max_sessions_vp9d(inst->core)) {
-				i_vpr_e(inst,
-					"%s: vp9d sessions exceeded max limit %d\n",
-					__func__, MAX_VP9D_INST_COUNT);
-				rc = -ENOMEM;
-				goto err_invalid_fmt;
-			}
 		}
 
 		fmt = &inst->fmts[INPUT_PORT];
