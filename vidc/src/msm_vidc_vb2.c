9f768879a3446eb6246fbe61e5e2a8e020929c19
Satyam Gupta <quic_satygupt@quicinc.com>
2024-09-25 09:32:20 +0530

    video: driver: fix random page_fault and misr mismatch issues
    [1] Smmu driver is always returning a unique iova(due to removal of
    DELAYED_UNMAP attr) for a given physical block, so every time generated
    iova(device_addr) is different (even though dma_buf addr is same). So
    lookup based on dma_buf addr during unmap & detach is going wrong and
    i.e leading to unmap/detach of unintended addresses and resulting into
    S1 page fault issues.
    
    [2] Entire RO handling at FW & driver is going for a toss due reason #1.
    For e.x, iova 0xdff00000(inode 701) is fed to decoder as FTB and got back
    FBD+RO, so 0xdff00000 is added to RO list and buffer is returned to client
    as FBD and next time client is requeuing the same block with different iova
    0xdfee0000(inode 701). Here since it is same physical block dma_buf addr
    will not change. Since it is different iova addr, iova lookup will fail in
    process_read_only_buffer(), so we are not attaching RO flag, while
    requeuing same buffer to FW as FTB, so FW treating that as plain buffer
    and overwriting the content, while its still being referenced, so i.e
    resulting into random MISR mismatch issues.
    
    Change-Id: I74004818030022fdc1d155eace7918793a7388b3
    Signed-off-by: Satyam Gupta <quic_satygupt@quicinc.com>

--- vidc/src/msm_vidc_vb2.c
+++ vidc/src/msm_vidc_vb2.c
@@ -65,6 +65,7 @@ void *msm_vb2_attach_dmabuf(struct vb2_buffer *vb, struct device *dev,
 	struct msm_vidc_inst *inst;
 	struct msm_vidc_core *core;
 	struct msm_vidc_buffer *buf = NULL;
+	struct msm_vidc_buffer *ro_buf, *dummy;
 
 	if (!vb || !dev || !dbuf || !vb->vb2_queue) {
 		d_vpr_e("%s: invalid params\n", __func__);
@@ -85,6 +86,18 @@ void *msm_vb2_attach_dmabuf(struct vb2_buffer *vb, struct device *dev,
 		goto exit;
 	}
 	buf->inst = inst;
+	buf->dmabuf = dbuf;
+
+	if (is_decode_session(inst) && is_output_buffer(buf->type)) {
+		list_for_each_entry_safe(ro_buf, dummy, &inst->buffers.read_only.list, list) {
+			if (ro_buf->dmabuf != buf->dmabuf)
+				continue;
+			print_vidc_buffer(VIDC_LOW, "low ", "attach: found ro buf", inst, ro_buf);
+			buf->attach = ro_buf->attach;
+			ro_buf->attach = NULL;
+			goto exit;
+		}
+	}
 
 	buf->attach = call_mem_op(core, dma_buf_attach, core, dbuf, dev);
 	if (!buf->attach) {
@@ -92,7 +105,6 @@ void *msm_vb2_attach_dmabuf(struct vb2_buffer *vb, struct device *dev,
 		buf = NULL;
 		goto exit;
 	}
-	buf->dmabuf = dbuf;
 	print_vidc_buffer(VIDC_LOW, "low ", "attach", inst, buf);
 
 exit:
@@ -132,13 +144,12 @@ void msm_vb2_detach_dmabuf(void *buf_priv)
 
 	if (is_decode_session(inst) && is_output_buffer(vbuf->type)) {
 		list_for_each_entry_safe(ro_buf, dummy, &inst->buffers.read_only.list, list) {
-			if (ro_buf->dmabuf == vbuf->dmabuf) {
-				print_vidc_buffer(VIDC_LOW, "low ", "detach: found ro buf",
-						  inst, ro_buf);
-				ro_buf->attach = vbuf->attach;
-				vbuf->attach = NULL;
-				goto exit;
-			}
+			if (ro_buf->dmabuf != vbuf->dmabuf)
+				continue;
+			print_vidc_buffer(VIDC_LOW, "low ", "detach: found ro buf", inst, ro_buf);
+			ro_buf->attach = vbuf->attach;
+			vbuf->attach = NULL;
+			goto exit;
 		}
 	}
 
@@ -146,12 +157,11 @@ void msm_vb2_detach_dmabuf(void *buf_priv)
 	if (vbuf->attach && vbuf->dmabuf) {
 		call_mem_op(core, dma_buf_detach, core, vbuf->dmabuf, vbuf->attach);
 		vbuf->attach = NULL;
-		vbuf->dmabuf = NULL;
-		vbuf->inst = NULL;
 	}
-	vbuf->inst = NULL;
 
 exit:
+	vbuf->dmabuf = NULL;
+	vbuf->inst = NULL;
 	return;
 }
 
@@ -161,6 +171,7 @@ int msm_vb2_map_dmabuf(void *buf_priv)
 	struct msm_vidc_buffer *buf = buf_priv;
 	struct msm_vidc_core *core;
 	struct msm_vidc_inst *inst;
+	struct msm_vidc_buffer *ro_buf, *dummy;
 
 	if (!buf || !buf->inst) {
 		d_vpr_e("%s: invalid params\n", __func__);
@@ -173,6 +184,18 @@ int msm_vb2_map_dmabuf(void *buf_priv)
 		return -EINVAL;
 	}
 	core = inst->core;
+
+	if (is_decode_session(inst) && is_output_buffer(buf->type)) {
+		list_for_each_entry_safe(ro_buf, dummy, &inst->buffers.read_only.list, list) {
+			if (ro_buf->dmabuf != buf->dmabuf)
+				continue;
+			print_vidc_buffer(VIDC_LOW, "low ", "map: found ro buf", inst, ro_buf);
+			buf->sg_table = ro_buf->sg_table;
+			buf->device_addr = ro_buf->device_addr;
+			ro_buf->sg_table = NULL;
+			goto exit;
+		}
+	}
 
 	buf->sg_table = call_mem_op(core, dma_buf_map_attachment, core, buf->attach);
 	if (!buf->sg_table || !buf->sg_table->sgl) {
@@ -210,15 +233,13 @@ void msm_vb2_unmap_dmabuf(void *buf_priv)
 
 	if (is_decode_session(inst) && is_output_buffer(vbuf->type)) {
 		list_for_each_entry_safe(ro_buf, dummy, &inst->buffers.read_only.list, list) {
-			if (ro_buf->dmabuf == vbuf->dmabuf) {
-				print_vidc_buffer(VIDC_LOW, "low ", "unmap: found ro buf",
-						  inst, ro_buf);
-				ro_buf->sg_table = vbuf->sg_table;
-				ro_buf->attach = vbuf->attach;
-				vbuf->sg_table = NULL;
-				vbuf->device_addr = 0x0;
-				goto exit;
-			}
+			if (ro_buf->dmabuf != vbuf->dmabuf)
+				continue;
+			print_vidc_buffer(VIDC_LOW, "low ", "unmap: found ro buf", inst, ro_buf);
+			ro_buf->sg_table = vbuf->sg_table;
+			vbuf->sg_table = NULL;
+			vbuf->device_addr = 0x0;
+			goto exit;
 		}
 	}
 
