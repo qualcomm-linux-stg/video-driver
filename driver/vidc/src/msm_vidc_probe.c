CodeLinaro-mirror/le_platform_vendor_opensource_video-driver

video: driver: enable codec function on x1e80100
add platform data for x1e80100 and keep the clk name same as upstream.

Change-Id: Id5828aa3238c3039f496b637e5c874d7600ebfcd
Signed-off-by: Renjiang Han <renjiang@qti.qualcomm.com>

driver/vidc/src/msm_vidc_probe.c
@@ -58,7 +58,8 @@ static inline bool is_video_device(struct device *dev)
 		of_device_is_compatible(dev->of_node, "qcom,sa8797-vidc") ||
 		of_device_is_compatible(dev->of_node, "qcom,niobe-vidc") ||
 		of_device_is_compatible(dev->of_node, "qcom,alor-vidc") ||
-		of_device_is_compatible(dev->of_node, "qcom,art-vidc"));
+		of_device_is_compatible(dev->of_node, "qcom,art-vidc") ||
+		of_device_is_compatible(dev->of_node, "qcom,x1e80100-vidc"));
 }
 
 static inline bool is_video_context_bank_device_node(struct device_node *of_node)
@@ -154,6 +155,7 @@ static const struct of_device_id msm_vidc_dt_match[] = {
 	{.compatible = "qcom,volcano-vidc"},
 	{.compatible = "qcom,niobe-vidc"},
 	{.compatible = "qcom,tuna-vidc"},
+	{.compatible = "qcom,x1e80100-vidc"},
 	{.compatible = "qcom,vidc,cb-ns-pxl"},
 	{.compatible = "qcom,vidc,cb-ns"},
 	{.compatible = "qcom,vidc,cb-ns-bitstream"},

qualcomm-linux-stg/video-driver

driver/vidc/src/msm_vidc_probe.c
@@ -57,7 +57,8 @@ static inline bool is_video_device(struct device *dev)
 		of_device_is_compatible(dev->of_node, "qcom,seraph-vidc") ||
 		of_device_is_compatible(dev->of_node, "qcom,sa8797-vidc") ||
 		of_device_is_compatible(dev->of_node, "qcom,niobe-vidc") ||
-		of_device_is_compatible(dev->of_node, "qcom,alor-vidc"));
+		of_device_is_compatible(dev->of_node, "qcom,alor-vidc") ||
+		of_device_is_compatible(dev->of_node, "qcom,x1e80100-vidc"));
 }
 
 static inline bool is_video_context_bank_device_node(struct device_node *of_node)
@@ -153,6 +154,7 @@ static const struct of_device_id msm_vidc_dt_match[] = {
 	{.compatible = "qcom,volcano-vidc"},
 	{.compatible = "qcom,niobe-vidc"},
 	{.compatible = "qcom,tuna-vidc"},
+	{.compatible = "qcom,x1e80100-vidc"},
 	{.compatible = "qcom,vidc,cb-ns-pxl"},
 	{.compatible = "qcom,vidc,cb-ns"},
 	{.compatible = "qcom,vidc,cb-ns-bitstream"},