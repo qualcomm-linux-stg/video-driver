47bcc5ea1aa69f7a3ba15a00dd794d1cff14d527
Manigurr <manigurr@qti.qualcomm.com>
2026-04-08 12:32:14 +0530

    updated kernel headers

--- video_kernel_headers.py
+++ video_kernel_headers.py
@@ -11,6 +11,10 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
 
 import argparse
 import filecmp
